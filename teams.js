23711f70aee44146b648d640e6eeb84d4cd77242
elefsaria <mitraponsel5828@gmail.com>
2025-06-28 02:53:55 +0700

    Create teams.js

---
+++ teams.js
@@ -0,0 +1,7 @@
+const teams = [
+  { name: "Pink", color: "#ff69b4" },
+  { name: "Purple", color: "#800080" },
+  { name: "Blue", color: "#1e90ff" },
+  { name: "Green", color: "#2ecc71" },
+  { name: "Orange", color: "#f39c12" },
+];
